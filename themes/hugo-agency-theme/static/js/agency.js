dc72c98b806191d3d7eafe609fdc9b3cdd938d32
charliecdb <52182450+charliecdb@users.noreply.github.com>
2019-06-26 22:51:01 -0400

    Update agency.js
    correccion errores

--- themes/hugo-agency-theme/static/js/agency.js
+++ themes/hugo-agency-theme/static/js/agency.js
@@ -52,18 +52,23 @@ $('form[id=contactForm]').submit(function(){
  //  $('form[id=contactForm] #success').hide();
 //    $('form[id=contactForm] #error').hide();
 //    if (jqXHR.status == 200) {
-      $('form[id=contactForm] #success').show();
+  //    $('form[id=contactForm] #success').show();
      //'json').fail(function(){
     //  $('form[id=contactForm] #success').hide();
     //  $('form[id=contactForm] #error').hide();
   //    $('form[id=contactForm] #error').show();
-  });
+  //});
 //  return false;
 //});
+  // Just remove the AJAX part and use the default submit behaviour from Netlify
+  $('form[id=contactForm] #success').show();
+});
 
+  
+  
 // Contact form validation
 $.validate({
-  modules : 'html5, //toggleDisabled'
+  modules : 'html5'
 });
 
 //function onContactCaptcha($form) {
